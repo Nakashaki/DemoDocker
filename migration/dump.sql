266bf1e160db24da48849f3c666c102f82ef6829
Salma WADOUACHI <s_wadouachi@hetic.eu>
2022-11-18 20:37:09 +0100

    création base de données

--- migration/dump.sql
+++ migration/dump.sql
@@ -1,4 +1,32 @@
-create table if not exists francishuster
+CREATE TABLE IF NOT EXISTS User
 (
-    id int not null primary key auto_increment
+    id        INT          NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    username  VARCHAR(255) NOT NULL,
+    password  VARCHAR(255) NOT NULL,
+    email     VARCHAR(255) NOT NULL,
+    firstName VARCHAR(255),
+    lastName  VARCHAR(255),
+    gender    CHAR(1),
+    roles     JSON         NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS Post
+(
+    id      INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    content TEXT,
+    author_id  int NOT NULL,
+    title varchar(255),
+    date datetime,
+    CONSTRAINT FK_author_id FOREIGN KEY (author_id) references User(id)
+);
+
+CREATE TABLE IF NOT EXISTS Comment
+(
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    content TEXT,
+    author_id int NOT NULL,
+    post_id int NOT NULL,
+    date datetime,
+    CONSTRAINT FK_post_id FOREIGN KEY (post_id) REFERENCES Post(id)
+);
+ALTER TABLE Comment ADD FOREIGN KEY (author_id) REFERENCES User(id);
