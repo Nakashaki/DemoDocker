6ae13e7c8e26c085a7cae03e64f24123d17eec62
Nakashaki <99727655+Nakashaki@users.noreply.github.com>
2022-11-18 10:38:11 +0100

    changement

--- app/index.php
+++ app/index.php
@@ -1,20 +1,20 @@
 <?php
 //use App\Factory\PDOFactory;
 //use App\Manager\UserManager;
-//session_start();
-//$pdo = new PDO("mysql:host=database;dbname=blog", "root", "password");
-//
-//require_once 'vendor/autoload.php';
-//
-//$url = "/" . trim(explode("?", $_SERVER['REQUEST_URI'])[0], "/");
-//
-////switch ($url) {
-////    case "/":
-////        $controller = new \App\Controller\PostController();
-////        $controller->home();
-////        break;
-//
-//
-//    default:
-//        throw new Exception("RIEN TROUVE !");
-//}
+session_start();
+$pdo = new PDO("mysql:host=database;dbname=blog", "root", "password");
+
+require_once 'vendor/autoload.php';
+
+$url = "/" . trim(explode("?", $_SERVER['REQUEST_URI'])[0], "/");
+
+switch ($url) {
+    case "/":
+        $controller = new \App\controller\PostController();
+        $controller->home();
+        break;
+
+
+    default:
+        throw new Exception("RIEN TROUVE !");
+}
