7463337256f66d57893b540b321d1c23f758ab96
Salma WADOUACHI <s_wadouachi@hetic.eu>
2022-11-18 14:42:09 +0100

    modification de méde

--- app/index.php
+++ app/index.php
@@ -1,20 +1,6 @@
 <?php
-//use App\Factory\PDOFactory;
-//use App\Manager\UserManager;
-session_start();
-$pdo = new PDO("mysql:host=database;dbname=blog", "root", "password");
-
-require_once 'vendor/autoload.php';
-
-$url = "/" . trim(explode("?", $_SERVER['REQUEST_URI'])[0], "/");
-
-switch ($url) {
-    case "/":
-        $controller = new \App\controller\PostController();
-        $controller->home();
-        break;
-
-
-    default:
-        throw new Exception("RIEN TROUVE !");
-}
+$pdo = new PDO("mysql:host=database;dbname=blog","root","password",
+    [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    ]);
+echo "coucou";
